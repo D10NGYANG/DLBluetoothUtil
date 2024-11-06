6c643d72df3c2edda6cf9f057551436465aa719a
D10NGYANG <D10NGYANG@163.com>
2024-11-06 11:43:30 +0800

    feat: 移除第三方Android蓝牙库；

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -38,8 +38,6 @@ kotlin {
             implementation(libs.androidx.startup.runtime)
             // 蓝牙通讯
             api(libs.androidx.bluetooth)
-            // 蓝牙通讯2
-            api("com.github.buhuiming:BleCore:2.3.2")
             // APP通用工具
             implementation(libs.dl.app)
         }
