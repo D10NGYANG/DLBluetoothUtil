2d0b5e7ff90a1ba70695e24b9d28120b348e2373
D10NGYANG <D10NGYANG@163.com>
2025-03-06 10:18:10 +0800

    chore(build): 更新 Gradle 和依赖版本
     更新构建工具和依赖库的版本以保持最新
     • 将 Gradle 版本从 8.7 升级到 8.13
     • 更新 Android 插件和其他相关库的版本
     • 添加依赖版本检查任务，确保使用的版本是稳定的

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,18 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
